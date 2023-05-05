5927e8dca1ce8b765fb6be71348178aa25684869
matias <murrutia2002@gmail.com>
2023-05-05 13:55:00 -0400

    parte1

---
+++ src/main/java/cl/unab/vina/paradigma/proyecto_2/Proyecto_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cl.unab.vina.paradigma.proyecto_2;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Proyecto_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
